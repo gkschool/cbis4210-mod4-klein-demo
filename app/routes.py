9baf6f240dcec478e20115f0e982ee18ca4ac00d
gkschool <george.klein@bobcats.gcsu.edu>
2024-11-24 01:54:50 -0500

    secondary commit

--- app/routes.py
+++ app/routes.py
@@ -9,3 +9,18 @@ def index():
 def about():
     return render_template('about.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
